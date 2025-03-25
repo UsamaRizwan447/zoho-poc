41345eca83bea3fa5614035511f18d85634a16a5
UsamaRizwan447 <uvirk447@gmail.com>
2025-03-25 12:48:37 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Flask Zoho POC is running"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
